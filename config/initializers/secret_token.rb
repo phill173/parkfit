4c16d9528ae1d630238e7cd1d2d1c95992bc0b71
Phillip Hayes <phillhayes6@hotmail.com>
2013-12-14 20:26:38 +0000

    Here is a rails application with added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkfit::Application.config.secret_token = 'a481e2b78ab179a45c5e2490ac3f7b568147294c4c7fec164c356ebdce012e2ab7294ff7c8704887b4734fed6465adf43634bb358132627e9c1d2d15e9fb9401'
