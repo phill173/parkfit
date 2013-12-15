e28f56154f905f54d3c4cf6cceb7a504ae29707e
Phillip Hayes <phillhayes6@hotmail.com>
2013-12-15 14:39:50 +0000

    Validation testing for status updates

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,8 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "Should be logged in to create StatusesControllerTest" do
+    post :create, status: {content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should create status" do
+
+  test "should create status when logged in" do
+    sign_in users(:phillip)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
@@ -37,16 +44,23 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:phillip)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect update status when not logged in" do
     put :update, id: @status, status: { content: @status.content}
-    assert_redirected_to status_path(assigns(:status))
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
+  test "SHould update status when logged in" do
+    sign_in users(:phillip)
+    put :update, id: @status, status: { content: @status.content}
+    assert_redirected_to status_path(assigns(:status)) 
+end
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
